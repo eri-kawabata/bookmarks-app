ba224ad46168f6a8782a8b78bae7e0cff54de880
eri-kawabata <eri.092127.k@gmail.com>
2025-01-02 16:39:59 +0900

    config

--- includes/config.php
+++ includes/config.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'localhost';
-$dbname = 'bookmarks';
-$username = 'root';
-$password = '';
+$host = 'mysql3104.db.sakura.ne.jp';
+$dbname = 'gs-erik_bookmarksapp';
+$username = 'gs-erik_bookmarksapp';
+$password = 'gintoki555';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
